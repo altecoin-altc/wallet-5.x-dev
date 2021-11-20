d351a0b24a66a61a25da6bb1a76b1bc427c08a06
Liquid <Liquid369@gmail.com>
2021-11-20 11:26:24 -0600

    Proper logging

--- src/consensus/tx_verify.cpp
+++ src/consensus/tx_verify.cpp
@@ -96,7 +96,7 @@ bool CheckTransaction(const CTransaction& tx, bool fZerocoinActive, CValidationS
         if (txout.IsEmpty() && !tx.IsCoinBase() && !tx.IsCoinStake())
             return state.DoS(100, false, REJECT_INVALID, "bad-txns-vout-empty");
         if (txout.nValue < 0)
-            LogPrintf("txout.nValue: %s", txout.nValue);
+            LogPrintf("txout.nValue: %s", FormatMoney(txout.nValue));
             return state.DoS(100, false, REJECT_INVALID, "bad-txns-vout-negative");
         if (txout.nValue > consensus.nMaxMoneyOut)
             return state.DoS(100, false, REJECT_INVALID, "bad-txns-vout-toolarge");
